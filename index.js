4de62ee5dde61dd68f70f3a3a1c4bbf6785c1b1d
panjiprann <panjipranantya20@gmail.com>
2025-10-28 10:31:16 +0700

    feat: add PUT endpoint for updating komik by ID

--- index.js
+++ index.js
@@ -39,5 +39,15 @@ app.get('/komiks', async (req, res) => {
     }
 });
 
+app.put('/komiks/:id', async (req, res) => {
+    const id = req.params.id;
+    const data = req.body;
+
+    try {
+        const komik = await db.Komik.findByPk(id);
+        if (!komik) {
+            return res.status(404).send({ message: 'Komik not found' });
+        }
+
 
           
